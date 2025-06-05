01f43cd7d3ed4a53b6b071c413a35fd2d5c01903
Stefano Esposito <stefano.esposito97@outlook.com>
2025-06-05 16:45:28 +0200

    fixed metric_depth run

---
+++ run_small.sh
@@ -0,0 +1,8 @@
+python3 run.py \
+    --input_video ./assets/example_videos/davis_rollercoaster.mp4 \
+    --output_dir ./outputs \
+    --encoder vits \
+    --input_size 518 \
+    --max_res 1280 \
+    --target_fps -1 \
+    --save_npz
